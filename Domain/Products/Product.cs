871d512207d8a461374a03cff35094e5dd3293f5
VitorSo lerA g Soler Aguilar <vitorsoleraguilar49@gmail.com>
2023-06-12 23:04:56 -0300

    Dominio produto

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -1,9 +1,12 @@
 ﻿namespace IWantApp.Domain.Products;
 
-public class Category
+public class Product
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public string Description { get; set; }
+    public bool HasStock { get; set; }
+    public Category Category { get; set; }
 
     public string CraetedBy { get; set; }
     public DateTime CreatedOn { get; set; }
