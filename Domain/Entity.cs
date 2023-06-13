ae5bd02f537ed381bb18b5aaa338f6503867790f
VitorSo lerA g Soler Aguilar <vitorsoleraguilar49@gmail.com>
2023-06-12 23:14:52 -0300

    Refatorando dominio

---
+++ Domain/Entity.cs
@@ -0,0 +1,18 @@
+﻿namespace IWantApp.Domain;
+
+public abstract class Entity
+{
+    public Entity()
+    {
+        // A classe ja cria um Id automaticamente para a classe que herdar seus atributos
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+
+    public string CraetedBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string EditedBy { get; set; }
+    public DateTime EditedOn { get; set; }
+}
--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -1,12 +1,6 @@
 ﻿namespace IWantApp.Domain.Products;
 
-public class Category
+public class Category : Entity
 {
-    public Guid Id { get; set; }
     public string Name { get; set; }
-
-    public string CraetedBy { get; set; }
-    public DateTime CreatedOn { get; set; }
-    public string EditedBy { get; set; }
-    public DateTime EditedOn { get; set; }
 }
