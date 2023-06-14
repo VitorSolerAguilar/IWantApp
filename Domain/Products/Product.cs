c5ce00feb4fff7de8e37e4a9e74881ef7ac3e29a
VitorSo lerA g Soler Aguilar <vitorsoleraguilar49@gmail.com>
2023-06-13 22:58:14 -0300

    Contexto ef

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -5,5 +5,7 @@ public class Product : Entity
     public string Name { get; set; }
     public string Description { get; set; }
     public bool HasStock { get; set; }
+
+    public int CategoryId { get; set; }
     public Category Category { get; set; }
 }
---
+++ Infra/Data/AplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using IWantApp.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace IWantApp.Infra.Data;
+
+public class AplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Product>().Property(p => p.Name).IsRequired();
+        builder.Entity<Product>().Property(p => p.Description).HasMaxLength(255);
+
+        builder.Entity<Category>().Property(c => c.Name).IsRequired();
+
+    }
+}
